f8ffa5f3ee25442d079213f06c0729af77737c15
objektwerks <objektwerks@runbox.com>
2023-11-08 11:15:53 -0500

    enhanced fury test

--- src/test/scala/objektwerks/FuryTest.scala
+++ src/test/scala/objektwerks/FuryTest.scala
@@ -1,8 +1,17 @@
 package objektwerks
 
+import io.fury.Fury
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 final class FuryTest extends AnyFunSuite with Matchers:
+  val fury = Fury
+    .builder
+    .withScalaOptimizationEnabled(true)
+    .requireClassRegistration(false)
+    .withRefTracking(true)
+    .build
+
   test("fury"):
     println("todo")
