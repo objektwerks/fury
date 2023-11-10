51309aafa4d69939014003d9a9bf8a2a148ed2f6
objektwerks <objektwerks@runbox.com>
2023-11-10 16:52:43 -0500

    edited jsoiniter test

--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -1,11 +1,7 @@
 package objektwerks
 
-import com.github.plokhotnyuk.jsoniter_scala.core.*
-
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-
-import Person.given
 
 final class JsoniterTest extends AnyFunSuite with Matchers:
   test("person"):  
