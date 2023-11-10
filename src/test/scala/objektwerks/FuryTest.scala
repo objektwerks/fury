d5ed8976c9341750ea1d8cf4817d4aa652ddd3fb
objektwerks <objektwerks@runbox.com>
2023-11-10 16:51:58 -0500

    refactored person, fury and jsoniter artifacts

--- src/test/scala/objektwerks/FuryTest.scala
+++ src/test/scala/objektwerks/FuryTest.scala
@@ -4,14 +4,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 final class FuryTest extends AnyFunSuite with Matchers:
-  test("case class"):
+  test("person"):
     val person = Person.newPerson
     val serializedPerson = Store.serialize(person)
     val deserializedPerson = Store.deserialize(serializedPerson)
     person shouldBe deserializedPerson
-
-  test("list of case classes"):
-    val persons = Person.newPersons
-    val serializedPersons = Store.serialize(persons)
-    val deserializedPersons = Store.deserialize(serializedPersons)
-    persons shouldBe deserializedPersons
--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -8,12 +8,7 @@ import org.scalatest.matchers.should.Matchers
 import Person.given
 
 final class JsoniterTest extends AnyFunSuite with Matchers:
-  test("case class"):  
+  test("person"):  
     val person = Person.newPerson
     val personJson = Store.toJson(person)
     person shouldBe Store.fromJson(personJson)
-
-  test("list of case classes"):
-    val persons = Person.newPersons
-    val personsJson = Store.toJson(persons)
-    persons shouldBe Store.fromJson(personsJson)
