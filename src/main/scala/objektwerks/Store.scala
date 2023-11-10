2c89fb32cc1b3e735d433ab0de295d9ed07e39e3
objektwerks <objektwerks@runbox.com>
2023-11-10 16:29:52 -0500

    added serialize def to store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1,4 +1,13 @@
 package objektwerks
 
+import io.fury.Fury
+
 object Store:
-  
+  val fury = Fury
+    .builder
+    .withScalaOptimizationEnabled(true)
+    .requireClassRegistration(false)
+    .withRefTracking(true)
+    .build
+
+  def serialize(person: Person): Array[Byte] = fury.serialize(person)
