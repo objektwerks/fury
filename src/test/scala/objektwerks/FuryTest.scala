45dfa48f322df6b0c4f88f09b890df9fcec7ae11
objektwerks <objektwerks@runbox.com>
2023-11-11 06:53:53 -0500

    enhanced fury test

--- src/test/scala/objektwerks/FuryTest.scala
+++ src/test/scala/objektwerks/FuryTest.scala
@@ -9,3 +9,9 @@ final class FuryTest extends AnyFunSuite with Matchers:
     val serializedPerson = Store.serialize(person)
     val deserializedPerson = Store.deserialize(serializedPerson)
     person shouldBe deserializedPerson
+
+  test("persons"):
+    val persons = List( Person.newPerson, Person.newPerson )
+    val serializedPersons = Store.serialize(persons)
+    val deserializedPersons = Store.deserialize(serializedPersons)
+    persons shouldBe deserializedPersons
